66816fb0c52b7ca48539f32c22242e5a8ee16622
Shaun McLellan <shaun.mclellan.fx@gmail.com>
2022-02-21 15:45:07 -0800

    make player move boundaries and padding

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -7,17 +7,40 @@ public class Player : MonoBehaviour
 {
     Vector2 rawInput;
 
+    Vector2 minBounds;
+    Vector2 maxBounds;
+    [SerializeField] float paddingLeft;
+    [SerializeField] float paddingRight;
+    [SerializeField] float paddingTop;
+    [SerializeField] float paddingBottom;
+
     [SerializeField] float moveSpeed = 10f;
+
+    void Start() 
+    {
+        InitBounds();
+    }
        
     void Update()
     {
-        Vector3 delta = Move();
-        transform.position += delta;
+        Move();
+        
     }
 
-    Vector3 Move()
+    void InitBounds()
     {
-        return rawInput * moveSpeed * Time.deltaTime;
+        Camera mainCamera = Camera.main;
+        minBounds = mainCamera.ViewportToWorldPoint(new Vector2(0,0));
+        maxBounds = mainCamera.ViewportToWorldPoint(new Vector2(1,1));
+    }
+
+    void Move()
+    {
+        Vector2 delta = rawInput * moveSpeed * Time.deltaTime;
+        Vector2 newPos = new Vector2();
+        newPos.x = Mathf.Clamp(transform.position.x + delta.x, minBounds.x + paddingLeft, maxBounds.x - paddingRight);
+        newPos.y = Mathf.Clamp(transform.position.y + delta.y, minBounds.y + paddingBottom, maxBounds.y - paddingTop);
+        transform.position = newPos;;
     }
 
     void OnMove(InputValue value)
