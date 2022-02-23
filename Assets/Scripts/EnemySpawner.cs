e0fd1622d71d6ed0b45f8441d14ef49a9f17742f
Shaun McLellan <shaun.mclellan.fx@gmail.com>
2022-02-23 14:59:12 -0800

    create some more waves

--- Assets/Scripts/EnemySpawner.cs
+++ Assets/Scripts/EnemySpawner.cs
@@ -7,13 +7,18 @@ public class EnemySpawner : MonoBehaviour
 
     [SerializeField] List<WaveConfigSO> waveConfigs;
     [SerializeField] int startingWave = 0;
+    [SerializeField] bool looping = false;
 
     // Start is called before the first frame update
-    void Start()
+    IEnumerator Start()
     {
-        
-        StartCoroutine(SpawnAllWaves());
+        do
+        {
+            yield return StartCoroutine(SpawnAllWaves());
+        }
+        while(looping);
     }
+        
 
     private IEnumerator SpawnAllWaves()
     {
